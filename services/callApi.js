6609eabc75d6f446fe987f7241ec580c9df4df61
Dale Seo <dale.seo@gmail.com>
2017-04-24 17:35:14 +0900

    responseText

--- services/callApi.js
+++ services/callApi.js
@@ -5,14 +5,17 @@ function callApi (req) {
   return superagent(req.method, req.url)
     .send(req.body)
     .then(res => {
-      console.log('<<< outgoing response:', res)
       return {
-        statusCode: res.statusCode,
-        statusText: res.statusText,
+        statusCode: res.res.statusCode,
+        statusMessage: res.res.statusMessage,
         headers: res.header,
         body: res.body,
         text: res.text
       }
+    })
+    .then(res => {
+      console.log('<<< outgoing response:', res)
+      return res
     })
 }
 
